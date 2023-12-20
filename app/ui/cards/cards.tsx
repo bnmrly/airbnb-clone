d085fa1249ffbd5a6c6ccc056e0f775a4ab47289
bnmrly <benjaminjohnmarley@gmail.com>
2023-12-20 20:34:33 +0000

    render cards

--- app/ui/cards/cards.tsx
+++ app/ui/cards/cards.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { fetchCardData } from "../../lib/data";
 import styles from "./cards.module.css";
 
@@ -10,26 +11,58 @@ export default async function Cards() {
     <>
       {!!staysData.length && (
         <div className={styles["airbnb-clone__cards-container"]}>
-          {" "}
-          <ul className={styles["airbnb-clone__cards-list"]}>
-            {staysData.map((stay: { title: string; photo: string }) => {
-              console.log("{staysData.map --- stay:", stay);
+          <ul className={styles["cards-list"]}>
+            {staysData.map(
+              (stay: {
+                title: string;
+                photo: string;
+                superHost: string;
+                type: string;
+                beds: string;
+                rating: string;
+              }) => {
+                return (
+                  <div className={styles["card"]} key={stay.photo}>
+                    <div
+                      className={styles["card__image-container"]}
+                      style={{
+                        backgroundImage: `url(${stay.photo})`,
+                        backgroundPosition: "center",
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat",
+                      }}
+                    ></div>
+                    <div className={styles["card__meta-container"]}>
+                      {stay.superHost && (
+                        <div className={styles["card__meta-superhost"]}>
+                          Superhost
+                        </div>
+                      )}
 
-              return (
-                <div
-                  className={styles["airbnb-mock__image-container"]}
-                  key={stay.photo}
-                  style={{
-                    backgroundImage: `url(${stay.photo})`,
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                  }}
-                >
-                  <p>{stay.title}</p>;
-                </div>
-              );
-            })}
+                      <p className={styles["card__meta-type"]}>{stay.type} </p>
+                      {stay.beds && (
+                        <p className={styles["card__meta-beds"]}>
+                          . {stay.beds} beds
+                        </p>
+                      )}
+                      <div className={styles["card-meta-rating__container"]}>
+                        <Image
+                          className={styles["airbnb-clone__rating-image"]}
+                          src="/star.svg"
+                          alt="star rating"
+                          height={17}
+                          width={16}
+                        />
+                        <p className={styles["card__meta-rating"]}>
+                          {stay.rating}
+                        </p>
+                      </div>
+                      <p className={styles["card__meta-title"]}>{stay.title}</p>
+                    </div>
+                  </div>
+                );
+              }
+            )}
           </ul>
         </div>
       )}
