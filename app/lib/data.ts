99cbbfcdedea877186fd575101098296ca83f41b
bnmrly <benjaminjohnmarley@gmail.com>
2024-01-21 08:21:37 +0000

    clean up fetchCardData

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,13 +5,27 @@ export async function fetchCardData() {
   const url = `https://gist.githubusercontent.com/bnmrly/017a87ef0b50c39e778c427a6b4bee60/raw/2f316eae029b7cedd6e0b808b07655e2d40f7281/holidays.json`;
 
   try {
-    return fetch(url)
-      .then((response) => response.json())
-      .then((staysData) => {
-        return { staysData };
-      });
+    const response = await fetch(url);
+    const staysData = await response.json();
+    return { staysData };
   } catch (error) {
     console.error("Error:", error);
     throw new Error("Failed to fetch card data.");
   }
 }
+
+// export async function fetchCardData() {
+//   noStore();
+//   const url = `https://gist.githubusercontent.com/bnmrly/017a87ef0b50c39e778c427a6b4bee60/raw/2f316eae029b7cedd6e0b808b07655e2d40f7281/holidays.json`;
+
+//   try {
+//     return fetch(url)
+//       .then((response) => response.json())
+//       .then((staysData) => {
+//         return { staysData };
+//       });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     throw new Error("Failed to fetch card data.");
+//   }
+// }
